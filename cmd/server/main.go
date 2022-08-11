c90867523cdaa59fd9c7e939516b16145f953c83
MERCER\lee-gordon <lee.gordon@mercer.com>
2022-08-11 14:00:46 +0100

    added health check endpoint

---
+++ cmd/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/gordone13/go-rest-api/internal/transport/http"
+)
+
+// App - the struct which contains things like pointers
+// to database connections
+type App struct{}
+
+// Run - sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting up Our App")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("GO REST API")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module example
+module github.com/gordone13/go-rest-api
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
